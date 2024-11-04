eff63ce3b00855a3e693b9e8b67e5b04aaaf75ef
Zaikin-Kirill <zaikin.ssau@yandex.ru>
2024-11-04 21:23:35 +0400

    module-6

---
+++ Module-6/Consumer/Consumer/Program.cs
@@ -0,0 +1,71 @@
+﻿using avro;
+using Avro.IO;
+using Avro.Specific;
+using Confluent.Kafka;
+
+namespace Consumer;
+
+class Program
+{
+    private static readonly DateTime start = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+    public static async Task Main(string[] args)
+    {
+        var consumerConfig = new ConsumerConfig
+        {
+            GroupId = "order-consumer-group",  // Уникальная группа потребителей
+            BootstrapServers = "localhost:9092",  // Адрес Kafka брокера
+            AutoOffsetReset = AutoOffsetReset.Earliest,  // Начало чтения с самого раннего сообщения
+            EnableAutoCommit = false  // Отключаем автоматическое коммитирование смещений
+        };
+
+        // Создание Consumer для получения сообщений
+        using var consumer = new ConsumerBuilder<Ignore, byte[]>(consumerConfig).Build();
+        consumer.Subscribe("order-topic");  // Подписка на топик
+
+        Console.WriteLine("Waiting for messages...");
+
+        while (true)
+        {
+            try
+            {
+                // Получение сообщения из Kafka
+                var consumeResult = consumer.Consume(CancellationToken.None);
+
+                // Десериализация сообщения вручную с использованием Avro
+                var order = DeserializeAvroMessage(consumeResult.Message.Value);
+
+                // Вывод данных пользователя
+                Console.WriteLine($"Received Order: ID = {order.orderID}, TotalCost = {order.totalCost}, CreatedAt = {start.AddSeconds(order.createdAt).ToLocalTime() }");
+                Console.WriteLine("Dishes: ");
+                foreach (var dish in order.dishesList)
+                {
+                    Console.WriteLine($"DishId = {dish.dishId}, Title = {dish.title}");
+                }
+
+                // Фиксируем смещение после успешной обработки сообщения
+                consumer.Commit(consumeResult);
+            }
+            catch (ConsumeException e)
+            {
+                Console.WriteLine($"Consume error: {e.Error.Reason}");
+            }
+        }
+    }
+
+    // Метод для десериализации Avro-сообщения
+    static Order DeserializeAvroMessage(byte[] avroData)
+    {
+        var orderSchema = Order._SCHEMA;
+
+        // Десериализация Avro-сообщения из двоичных данных
+        using (var stream = new MemoryStream(avroData))
+        {
+            var reader = new BinaryDecoder(stream);
+            var datumReader = new SpecificDatumReader<Order>(orderSchema, orderSchema);
+
+            // Чтение и десериализация данных
+            return datumReader.Read(null, reader);
+        }
+    }
+}
---
+++ Module-6/Producer/Producer/Program.cs
@@ -0,0 +1,82 @@
+﻿using avro;
+using Avro.IO;
+using Avro.Specific;
+using Confluent.Kafka;
+
+namespace Producer;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var order1 = new Order
+        {
+            orderID = 1,
+            createdAt = 1730730732,
+            totalCost = 500,
+            dishesList = new List<Dishes>
+            {
+                new() { dishId = 1, title = "Salad"},
+                new() { dishId = 2, title = "First dish"}
+            }
+        };
+        
+        var order2 = new Order
+        {
+            orderID = 2,
+            createdAt = 1730730855,
+            totalCost = 600,
+            dishesList = new List<Dishes>
+            {
+                new() { dishId = 1, title = "Salad"},
+                new() { dishId = 3, title = "Second dish"}
+            }
+        };
+        
+        // Сериализация объекта User в байты с помощью Avro
+        byte[] avroData1;
+        using (var ms = new MemoryStream())
+        {
+            var writer = new BinaryEncoder(ms);
+            var datumWriter = new SpecificDatumWriter<Order>(order1.Schema);
+            datumWriter.Write(order1, writer);
+            avroData1 = ms.ToArray();
+        }
+        
+        byte[] avroData2;
+        using (var ms = new MemoryStream())
+        {
+            var writer = new BinaryEncoder(ms);
+            var datumWriter = new SpecificDatumWriter<Order>(order2.Schema);
+            datumWriter.Write(order2, writer);
+            avroData2 = ms.ToArray();
+        }
+        
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            LingerMs = 5,  // Задержка в миллисекундах перед отправкой пакета сообщений
+            BatchSize = 16384, // Размер пакета в байтах
+            RequestTimeoutMs = 10000 // Таймаут операции отправки запроса в Kafka
+        };
+
+        using (var producer = new ProducerBuilder<Null, byte[]>(config).Build())
+        {
+            var message = new Message<Null, byte[]>
+            {
+                Value = avroData1
+            };
+
+            var deliveryResult = producer.ProduceAsync("order-topic", message).GetAwaiter().GetResult();
+            Console.WriteLine($"Message delivered to {deliveryResult.TopicPartitionOffset}");
+            
+            message = new Message<Null, byte[]>
+            {
+                Value = avroData2
+            };
+
+            deliveryResult = producer.ProduceAsync("order-topic", message).GetAwaiter().GetResult();
+            Console.WriteLine($"Message delivered to {deliveryResult.TopicPartitionOffset}");
+        }
+    }
+}
